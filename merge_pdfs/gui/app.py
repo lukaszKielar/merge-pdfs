02888063b3a1189da545c548c641e064754b3d58
Lukasz Kielar <kielar.lukasz@hotmail.com>
2021-02-09 21:58:40 +0100

    add support for pdf previews

--- merge_pdfs/gui/app.py
+++ merge_pdfs/gui/app.py
@@ -1,22 +1,40 @@
 from pathlib import Path
+
 from kivy.app import App as KivyApp
 from kivy.uix.boxlayout import BoxLayout
 
 from merge_pdfs.backend.app_data import AppData
+from merge_pdfs.backend.previews import PreviewRenderer
 
 
 APP_DATA = AppData()
 
 
 class MyWidget(BoxLayout):
-    def selected(self, filename):
-        try:
-            self.ids.image.source = filename[0]
-        except:
-            pass
+    def selected(self, path: str) -> None:
+        _path = Path(path[0])
+
+        # skip if it's folder
+        if _path.is_dir():
+            return
+
+        _extension = _path.suffix.lower()
+
+        if _extension == ".pdf":
+            preview_generator = PreviewRenderer(_path)
+            first_page_preview = preview_generator.render_preview(page=0)
+            self.ids.image.source = str(first_page_preview)
+            preview_generator.clean_up()
+        # show all files we can, including all image formats
+        else:
+            try:
+                self.ids.image.source = str(_path)
+            # skip when error
+            except:
+                return
 
     def default_path(self) -> Path:
-        return str(Path.cwd)
+        return str(Path.cwd())
 
 
 class App(KivyApp):
