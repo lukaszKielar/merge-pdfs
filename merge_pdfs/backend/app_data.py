d6b1048cbb6c3e2d164ccdb555140529c69e7e3a
Lukasz Kielar <kielar.lukasz@hotmail.com>
2021-02-24 20:27:44 +0100

    use AppData

--- merge_pdfs/backend/app_data.py
+++ merge_pdfs/backend/app_data.py
@@ -1,26 +1,50 @@
 import os
+import json
 import platform
 from pathlib import Path
+from typing import Any, Dict, Optional
 
 
 class AppData:
     """Class that represents Application Data files."""
 
     def __init__(self):
-        self._system = platform.system()
         self._create_app_data_folder()
 
+        self._settings = self._load_settings()
+
+    def __getattr__(self, attr: str) -> Optional[Any]:
+        return self._settings.get(attr, None)
+
     def _create_app_data_folder(self) -> None:
-        Path.mkdir(self.path, exist_ok=True)
+        Path.mkdir(self._path, exist_ok=True)
 
     @property
-    def path(self) -> Path:
+    def _system(self) -> str:
+        return platform.system()
+
+    @property
+    def _path(self) -> Path:
         if self._system == "Windows":
             _path = Path(os.getenv("APPDATA")) / "MergePDFs"
         else:
             _path = Path(os.getenv("HOME")) / ".mergepdfs"
         return _path
 
-    # TODO: implement this method
-    def save_cache(self):
-        pass
+    @property
+    def _settings_file(self) -> Path:
+        return self._path / "settings.json"
+
+    def _load_settings(self) -> Dict[str, Any]:
+
+        if not self._settings_file.exists():
+            return {}
+
+        with open(self._settings_file, "r") as f:
+            return json.load(f)
+
+    def save_setting(self, attr: str, value: Any) -> None:
+        self._settings[attr] = value
+
+        with open(self._settings_file, "w") as f:
+            json.dump(self._settings, f, indent=2)
