b21f13ab8ffe4a0dea0ee2783520d8f69dcef4e9
Lukasz Kielar <kielar.lukasz@hotmail.com>
2021-03-04 22:23:51 +0100

    change not implemented

--- merge_pdfs/backend/utils.py
+++ merge_pdfs/backend/utils.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 def not_implemented(func):
     @wraps(func)
-    def inner(*args, **kwargs):
+    def inner():
         print(f"{func.__name__} is not implemented yet!")
 
     return inner
