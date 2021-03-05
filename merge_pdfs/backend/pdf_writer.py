e96dce9a8f968316bcbc41c0434211eb04649ec8
Lukasz Kielar <kielar.lukasz@hotmail.com>
2021-03-05 23:30:25 +0100

    add logging to PdfWriter

--- merge_pdfs/backend/pdf_writer.py
+++ merge_pdfs/backend/pdf_writer.py
@@ -1,7 +1,10 @@
+import logging
 from pathlib import Path
 from typing import List, Union
 
 from PyPDF4 import PdfFileMerger
+
+logger = logging.getLogger(__name__)
 
 
 class PDFWriter:
@@ -9,16 +12,18 @@ class PDFWriter:
         self._merger = PdfFileMerger()
 
     def merge_files(self, *pdf_files: List[Union[str, Path]]) -> None:
+        logger.debug("Merging %d files", len(pdf_files))
         for pdf in pdf_files:
             with open(pdf, "rb") as f:
                 self._merger.append(f)
 
     def save(self, out_path: Union[str, Path]) -> Path:
+        logger.debug("Saving '%s' file", out_path)
         # add extension if doesn't exist
         if not str(out_path).lower().endswith(".pdf"):
             out_path += ".pdf"
 
-        with open(out_path, "wb") as f:
-            self._merger.write(f)
+        with open(out_path, "wb") as fd:
+            self._merger.write(fd)
 
         return Path(out_path)
