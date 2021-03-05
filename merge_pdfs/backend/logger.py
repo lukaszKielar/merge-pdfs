cabd78fdb0273faf634d98932a403ef03056be05
Lukasz Kielar <kielar.lukasz@hotmail.com>
2021-03-05 21:16:33 +0100

    define non-blocking logger

---
+++ merge_pdfs/backend/logger.py
@@ -0,0 +1,39 @@
+import logging
+import sys
+from logging.handlers import QueueHandler, QueueListener
+from queue import SimpleQueue
+
+
+class AppLogger:
+    @classmethod
+    def default(cls) -> logging.Logger:
+        """Defines non-blocking application logger.
+        Inspiration: https://www.zopatista.com/python/2019/05/11/asyncio-logging/
+
+        Returns:
+            logging.Logger: Root logger
+        """
+        # get root logger
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+            fmt="%(asctime)s - %(levelname)8s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+
+        stdout_handler = logging.StreamHandler(stream=sys.stdout)
+        stdout_handler.setLevel(logging.DEBUG)
+        stdout_handler.setFormatter(formatter)
+
+        queue = SimpleQueue()
+        queue_handler = QueueHandler(queue)
+        logger.addHandler(queue_handler)
+
+        listener = logging.handlers.QueueListener(
+            queue, *[stdout_handler], respect_handler_level=True
+        )
+
+        listener.start()
+
+        return logger
