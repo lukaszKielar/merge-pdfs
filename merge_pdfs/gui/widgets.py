336200aa320b57e2d207622dcb7c4935393ad47c
Lukasz Kielar <kielar.lukasz@hotmail.com>
2021-03-05 22:43:24 +0100

    add files from dialog

--- merge_pdfs/gui/widgets.py
+++ merge_pdfs/gui/widgets.py
@@ -1,9 +1,15 @@
+import logging
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict
 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QAbstractItemView, QListWidget, QListWidgetItem
+from PyQt5.QtWidgets import QAbstractItemView, QFileDialog, QListWidget, QListWidgetItem
+
+from merge_pdfs.backend.app_data import APP_DATA
+from merge_pdfs.backend.utils import not_implemented
+
+logger = logging.getLogger(__name__)
 
 
 class PDFListWidget(QListWidget):
@@ -18,6 +24,7 @@ class PDFListWidget(QListWidget):
         self.setAlternatingRowColors(True)
         self.setSelectionMode(QAbstractItemView.ContiguousSelection)
 
+        # dict that maps item label with its paths
         self._addedFiles: Dict[str, Path] = {}
 
     def dragEnterEvent(self, e: QtGui.QDragEnterEvent) -> None:
@@ -53,6 +60,7 @@ class PDFListWidget(QListWidget):
                             newFiles.append(_newFilePath.name)
 
                 self.addItems(newFiles)
+                logger.debug(self._addedFiles)
             else:
                 event.ignore()
 
@@ -72,3 +80,40 @@ class PDFListWidget(QListWidget):
         selectedItems = self.selectedItems()
         for item in selectedItems:
             self._removeItem(item)
+
+    def _addItem(self, item: str):
+        itemPath = Path(item)
+        itemText = itemPath.name
+
+        if itemText not in self._addedFiles:
+            logger.debug("Adding %s to the list", itemText)
+            self._addedFiles[itemText] = itemPath
+            self.addItem(itemText)
+        else:
+            logger.warning("Ignoring %s. Already on a list", itemText)
+
+    def addItemsFromDialog(self):
+        if APP_DATA.lastOpenedDir is None:
+            openDir = str(Path.home())
+        else:
+            openDir = APP_DATA.lastOpenedDir
+
+        selectedFiles, _ = QFileDialog.getOpenFileNames(
+            self,
+            "Add file(s)",
+            openDir,
+            "PDF files (*.pdf *.PDF *.Pdf)",
+            options=QFileDialog.DontUseNativeDialog,
+        )
+
+        if selectedFiles:
+            # save lastOpenedDir in settings file
+            APP_DATA.save_setting("lastOpenedDir", str(Path(selectedFiles[0]).parent))
+
+            logger.debug("%d files will be added", len(selectedFiles))
+            for _file in selectedFiles:
+                self._addItem(_file)
+
+    @not_implemented
+    def saveFile(self):
+        pass
