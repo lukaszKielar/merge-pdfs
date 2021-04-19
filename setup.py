ddd9797944b7ab78d879df610676e81a2e6abd24
Lukasz Kielar <kielar.lukasz@hotmail.com>
2021-04-19 21:35:27 +0200

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+import sys
+
 from cx_Freeze import Executable, setup
 
+base = "Win32GUI" if sys.version == "win32" else None
+
 setup(
-    name="MergePDFs",
+    name="merge_pdfs",
     version="0.0.1",
     author="Lukasz Kielar",
     description="Tool for merging PDF files without sending them over the Network",
-    executables=[Executable("merge_pdfs/gui/app.py", base="Win32GUI")],
+    executables=[Executable("merge_pdfs/gui/app.py", base=base)],
 )
